2c88c860547dfea7ef510237115d45be10c599ee
Wen Kokke <wenkokke@users.noreply.github.com>
2026-07-02 16:28:33 +0100

    Add source for ipedb version 0.2.
    Co-authored-by: fendor <fendor@users.noreply.github.com>

---
+++ src/IpeDB/Types/CostCentre.hs
@@ -0,0 +1,130 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+{- |
+Module      : IpeDb.Types.CostCentre
+Description : Representation for GHC cost centres.
+Stability   : experimental
+Portability : portable
+-}
+module IpeDB.Types.CostCentre (
+  CostCentreId (..),
+  CostCentre (..),
+  toCostCentre,
+  defaultCostCentreLSMTreeTableSpec,
+  defaultCostCentreIndexerOptions,
+) where
+
+import Control.Exception (assert)
+import Data.Binary (Binary (..), Get, Put)
+import Data.Binary.Text (getTextUtf8, putTextUtf8)
+import Data.Coerce (coerce)
+import Data.Default (Default (..))
+import Data.Either (fromRight, isRight)
+import Data.Hashable (Hashable)
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Word (Word32)
+import Database.LSMTree qualified as LSMT
+import Foreign (toBool)
+import Foreign.C.Types (CBool (..))
+import GHC.RTS.Events (Event)
+import GHC.RTS.Events qualified as E
+import IpeDB.Database qualified as DB
+import IpeDB.Types.SrcLoc (SrcLoc (..), parseSrcLoc)
+
+{- |
+The type of cost-centre IDs.
+-}
+newtype CostCentreId = CostCentreId
+  { value :: Word32
+  }
+  deriving newtype (Show, Eq, Ord, Hashable)
+
+{- |
+The type of a cost-centre entry, as produced by the `GHC.RTS.Events.HeapProfCostCentre` event.
+-}
+data CostCentre = CostCentre
+  { ccLabel :: !Text
+  , ccModule :: !Text
+  , ccSrcLoc :: !SrcLoc
+  , ccIsCAF :: !Bool
+  }
+  deriving stock (Show, Eq)
+
+{- |
+Extract a `CostCentre` from a @ghc-events@ `Event`.
+-}
+toCostCentre :: Event -> Maybe (CostCentreId, CostCentre)
+toCostCentre ev
+  -- If the event is an `E.InfoTableProv` event, process it, and set @started@...
+  | E.HeapProfCostCentre{..} <- ev.evSpec = do
+      let !ccId = CostCentreId heapProfCostCentreId
+      let !ccSrcLocOrError
+            | heapProfSrcLoc `elem` ["<built-in>", "<entire-module>"] = Right UnhelpfulSrcLoc
+            | otherwise = parseSrcLoc . T.unpack $ heapProfSrcLoc
+      assert (isRight ccSrcLocOrError) $ do
+        let !cc =
+              CostCentre
+                { ccLabel = heapProfLabel
+                , ccModule = heapProfModule
+                , ccSrcLoc = fromRight UnhelpfulSrcLoc ccSrcLocOrError
+                , ccIsCAF = toBool (coerce @E.HeapProfFlags @CBool heapProfFlags)
+                }
+        Just (ccId, cc)
+  | otherwise = Nothing
+
+{- |
+Default `DB.TableSpec` for CostCentre tables.
+-}
+defaultCostCentreLSMTreeTableSpec :: DB.TableSpec CostCentreId CostCentre
+defaultCostCentreLSMTreeTableSpec =
+  DB.LSMTreeTableSpec
+    { name = "cost-centre-table"
+    , label = "CostCentreId->CostCentre"
+    }
+
+instance Default (DB.TableSpec CostCentreId CostCentre) where
+  def :: DB.TableSpec CostCentreId CostCentre
+  def = defaultCostCentreLSMTreeTableSpec
+
+{- |
+Default `DB.IndexerOptions` for CostCentre tables.
+-}
+defaultCostCentreIndexerOptions :: DB.IndexerOptions Event CostCentreId CostCentre
+defaultCostCentreIndexerOptions = DB.defaultIndexerOptions toCostCentre
+
+instance Default (DB.IndexerOptions Event CostCentreId CostCentre) where
+  def :: DB.IndexerOptions Event CostCentreId CostCentre
+  def = defaultCostCentreIndexerOptions
+
+--------------------------------------------------------------------------------
+-- Instances for binary serialisation of CostCentre
+--------------------------------------------------------------------------------
+
+deriving newtype instance Binary CostCentreId
+
+instance Binary CostCentre where
+  get :: Get CostCentre
+  get = do
+    ccLabel <- getTextUtf8
+    ccModule <- getTextUtf8
+    ccSrcLoc <- get
+    ccIsCAF <- get
+    pure CostCentre{..}
+
+  put :: CostCentre -> Put
+  put CostCentre{..} = do
+    putTextUtf8 ccLabel
+    putTextUtf8 ccModule
+    put ccSrcLoc
+    put ccIsCAF
+
+--------------------------------------------------------------------------------
+-- Instances for lsm-tree serialisation of CostCentre
+--------------------------------------------------------------------------------
+
+deriving newtype instance LSMT.SerialiseKey CostCentreId
+
+deriving via DB.SerialiseViaBinary CostCentre instance LSMT.SerialiseValue CostCentre
+
+deriving via LSMT.ResolveAsFirst CostCentre instance LSMT.ResolveValue CostCentre
---
+++ src/IpeDB/Types/SrcLoc.hs
@@ -0,0 +1,367 @@
+{- |
+Module      : IpeDB.Types.SrcLoc
+Description : Representation for GHC source locations.
+Stability   : experimental
+Portability : portable
+-}
+module IpeDB.Types.SrcLoc (
+  SrcLoc (SrcLoc, UnhelpfulSrcLoc, srcFilePath, srcRange),
+  ppSrcLoc,
+  pSrcLoc,
+  parseSrcLoc,
+  Range (Range, start, end, ..),
+  Point (..),
+) where
+
+import Codec.LEB128.Generic (decodeLEB128, encodeLEB128)
+import Data.Binary (Binary (..), Get, Put, getWord8, putWord8)
+import Data.Binary.Text (getTextUtf8, putTextUtf8)
+import Data.Char (isDigit)
+import Data.Maybe (fromMaybe)
+import Data.Text qualified as T
+import Data.Word (Word32)
+import GHC.Records (HasField (..))
+import Text.ParserCombinators.ReadP (ReadP)
+import Text.ParserCombinators.ReadP qualified as P
+
+--------------------------------------------------------------------------------
+-- Source Locations
+--------------------------------------------------------------------------------
+
+{- |
+The type of source location information.
+-}
+data SrcLoc = SrcLoc_
+  { srcFilePath :: !(Maybe FilePath)
+  -- ^ @srcFilePath@ should not be @Just ""@.
+  , srcRange :: !(Maybe Range)
+  }
+  deriving stock (Eq)
+
+{- |
+Internal helper.
+
+Smart constructor for `SrcLoc`.
+-}
+toSrcLoc :: Maybe FilePath -> Maybe Range -> SrcLoc
+toSrcLoc srcFilePath =
+  SrcLoc_ (if srcFilePath == Just "" then Nothing else srcFilePath)
+
+{- |
+Smart constructor for `SrcLoc` that ensures that `srcFilePath` is not @Just ""@.
+-}
+pattern SrcLoc :: Maybe FilePath -> Maybe Range -> SrcLoc
+pattern SrcLoc{srcFilePath, srcRange} <- SrcLoc_ srcFilePath srcRange
+  where
+    SrcLoc srcFilePath srcRange = toSrcLoc srcFilePath srcRange
+
+{-# COMPLETE SrcLoc #-}
+
+{- |
+Simple constructor for unhelpful source locations.
+-}
+pattern UnhelpfulSrcLoc :: SrcLoc
+pattern UnhelpfulSrcLoc = SrcLoc_ Nothing Nothing
+
+instance Show SrcLoc where
+  showsPrec :: Int -> SrcLoc -> ShowS
+  showsPrec _ UnhelpfulSrcLoc =
+    showString "UnhelpfulSrcLoc"
+  showsPrec p (SrcLoc srcFilePath srcRange) =
+    showParen (p >= 11) $
+      showString "SrcLoc {"
+        . showString "srcFilePath = "
+        . shows srcFilePath
+        . showString ", "
+        . showString "srcRange = "
+        . shows srcRange
+        . showString "}"
+
+{- |
+Pretty-printer for GHC source location information
+-}
+ppSrcLoc :: SrcLoc -> ShowS
+ppSrcLoc SrcLoc{..} =
+  maybe mempty showString srcFilePath . showString ":" . maybe mempty ppRange srcRange
+
+{- |
+Parser for GHC source location information.
+-}
+pSrcLoc :: ReadP SrcLoc
+pSrcLoc = SrcLoc <$> pSrcFilePath <* P.char ':' <*> pSrcRange
+ where
+  -- TODO: This is very slow. Unfortunately, the format of GHC source
+  --       location information is a bit of a nightmare, and you can't
+  --       know where the filepath ends until you've successfully parsed
+  --       the source range, since the filepath and the range are separated
+  --       by a colon, but filepaths may contain any number of colons and
+  --       ranges may contain either zero or one colon.
+  pSrcFilePath :: ReadP (Maybe FilePath)
+  pSrcFilePath = Just <$> P.many P.get
+
+  pSrcRange :: ReadP (Maybe Range)
+  pSrcRange = (Just <$> pRange) P.<++ (Nothing <$ P.eof)
+
+{- |
+Parser for GHC source location information.
+-}
+parseSrcLoc :: String -> Either String SrcLoc
+parseSrcLoc s =
+  case [x | (x, "") <- P.readP_to_S pSrcLoc s] of
+    [x] -> Right x
+    [] -> Left "parseSrcLoc: no parse"
+    _ -> Left "parseSrcLoc: ambiguous parse"
+
+--------------------------------------------------------------------------------
+-- Source Ranges
+--------------------------------------------------------------------------------
+
+{- |
+A position in a source file.
+-}
+data Point
+  = Point
+  { line :: !Word32
+  , column :: !Word32
+  }
+  deriving (Eq, Ord)
+
+{- |
+A range in a source file.
+-}
+data Range
+  = Range'Point
+      { line :: !Word32
+      , column :: !Word32
+      }
+  | Range'OneLine
+      { line :: !Word32
+      , column :: !Word32
+      , endColumn :: !Word32
+      }
+  | Range'MultiLine
+      { line :: !Word32
+      , column :: !Word32
+      , endLine :: !Word32
+      , endColumn :: !Word32
+      }
+  deriving stock (Eq, Show)
+
+{- |
+Smart constructor for ranges.
+-}
+toRange :: (Point, Point) -> Range
+toRange (start@Point{..}, end)
+  | start == end = Range'Point{..}
+  | line == end.line = Range'OneLine{endColumn = end.column, ..}
+  | otherwise = Range'MultiLine{endLine = end.line, endColumn = end.column, ..}
+
+{- |
+Smart destructor for ranges.
+-}
+fromRange :: Range -> (Point, Point)
+fromRange r = (r.start, r.end)
+
+{- |
+Smart constructor for ranges.
+-}
+pattern Range :: Point -> Point -> Range
+pattern Range{start, end} <- (fromRange -> (start, end))
+  where
+    Range start end = toRange (start, end)
+
+{-# COMPLETE Range #-}
+
+{- |
+Get the start position of a range.
+-}
+instance HasField "start" Range Point where
+  getField :: Range -> Point
+  getField = \case
+    Range'Point{..} -> Point{..}
+    Range'OneLine{..} -> Point{..}
+    Range'MultiLine{..} -> Point{..}
+
+{- |
+Get the end position of a range.
+-}
+instance HasField "end" Range Point where
+  getField :: Range -> Point
+  getField = \case
+    Range'Point{..} -> Point{..}
+    Range'OneLine{..} -> Point{column = endColumn, ..}
+    Range'MultiLine{..} -> Point{line = endLine, column = endColumn}
+
+instance Semigroup Range where
+  (<>) :: Range -> Range -> Range
+  r1 <> r2 = Range (r1.start `min` r2.start) (r1.end `max` r2.end)
+
+{- Note [Parsing and Pretty-Printing]
+The parser and pretty-printer are based on the following pretty-printer for @RealSrcSpan@ in the GHC source:
+https://github.com/ghc/ghc/blob/674858e3fb1b7fe644d7ec5d4b0c6d9f18d7bc51/compiler/GHC/Types/SrcLoc.hs#L752-L774
+-}
+
+{- |
+Internal helper.
+
+Pretty-printer for ranges.
+-}
+ppRange :: Range -> ShowS
+ppRange = \case
+  Range'Point{..} ->
+    shows line . showString ":" . shows column
+  Range'OneLine{..} ->
+    shows line . showString ":" . shows column . showString "-" . shows endColumn
+  Range{..} ->
+    ppPoint start . showString "-" . ppPoint end
+ where
+  ppPoint :: Point -> ShowS
+  ppPoint p =
+    showParen True (shows p.line . showString "," . shows p.column)
+
+{- |
+Internal helper.
+
+Parser for ranges.
+-}
+pRange :: ReadP Range
+pRange = pRange'MultiLine P.<++ (pRange'OneLine P.+++ pPointRange)
+ where
+  pRange'MultiLine :: ReadP Range
+  pRange'MultiLine = Range <$> pPoint <* P.char '-' <*> pPoint
+
+  pRange'OneLine :: ReadP Range
+  pRange'OneLine = Range'OneLine <$> pIndex <* P.char ':' <*> pIndex <* P.char '-' <*> pIndex
+
+  pPointRange :: ReadP Range
+  pPointRange = Range'Point <$> pIndex <* P.char ':' <*> pIndex
+
+  pIndex :: ReadP Word32
+  pIndex = read <$> P.munch1 isDigit
+
+  pPoint :: ReadP Point
+  pPoint = Point <$ P.char '(' <*> pIndex <* P.char ',' <*> pIndex <* P.char ')'
+
+--------------------------------------------------------------------------------
+-- Instances for binary serialisation of SrcLoc
+--------------------------------------------------------------------------------
+
+{- |
+Internal helper.
+
+Serialise source location information.
+-}
+putSrcLoc :: SrcLoc -> Put
+putSrcLoc SrcLoc{..} = do
+  putTextUtf8 . T.pack . fromMaybe "" $ srcFilePath
+  putMaybeRange srcRange
+
+{- |
+Internal helper.
+
+Deserialise source location information.
+-}
+getSrcLoc :: Get SrcLoc
+getSrcLoc = do
+  srcFilePath <- Just . T.unpack <$> getTextUtf8
+  srcRange <- getMaybeRange
+  pure SrcLoc{..}
+
+instance Binary SrcLoc where
+  put :: SrcLoc -> Put
+  put = putSrcLoc
+  get :: Get SrcLoc
+  get = getSrcLoc
+
+--------------------------------------------------------------------------------
+-- Instances for binary serialisation of optional ranges
+--------------------------------------------------------------------------------
+
+{- |
+Internal helper.
+
+Constructor tags for binary serialisation of ranges.
+-}
+data MaybeRangeTag
+  = TagNothing
+  | TagJustRange'Point
+  | TagJustRange'OneLine
+  | TagJustRange'MultiLine
+
+{- |
+Internal helper.
+
+Serialise a constructor tag for an optional range.
+-}
+putMaybeRangeTag :: MaybeRangeTag -> Put
+putMaybeRangeTag = \case
+  TagNothing -> putWord8 0x01
+  TagJustRange'Point -> putWord8 0x02
+  TagJustRange'OneLine -> putWord8 0x03
+  TagJustRange'MultiLine -> putWord8 0x04
+
+{- |
+Internal helper.
+
+Deserialise a constructor tag for an optional range.
+-}
+getMaybeRangeTag :: Get MaybeRangeTag
+getMaybeRangeTag = do
+  maybeRangeTag <- getWord8
+  case maybeRangeTag of
+    0x01 -> pure TagNothing
+    0x02 -> pure TagJustRange'Point
+    0x03 -> pure TagJustRange'OneLine
+    0x04 -> pure TagJustRange'MultiLine
+    _otherwise -> fail $ "Unexpected tag " <> show maybeRangeTag
+
+{- |
+Internal helper.
+
+Serialise an optional range to binary.
+-}
+putMaybeRange :: Maybe Range -> Put
+putMaybeRange = \case
+  Nothing ->
+    putMaybeRangeTag TagNothing
+  Just Range'Point{..} -> do
+    putMaybeRangeTag TagJustRange'Point
+    encodeLEB128 putWord8 line
+    encodeLEB128 putWord8 column
+  Just Range'OneLine{..} -> do
+    putMaybeRangeTag TagJustRange'OneLine
+    encodeLEB128 putWord8 line
+    encodeLEB128 putWord8 column
+    encodeLEB128 putWord8 endColumn
+  Just Range'MultiLine{..} -> do
+    putMaybeRangeTag TagJustRange'MultiLine
+    encodeLEB128 putWord8 line
+    encodeLEB128 putWord8 column
+    encodeLEB128 putWord8 endLine
+    encodeLEB128 putWord8 endColumn
+
+{- |
+Internal helper.
+
+Deserialise an optional range from binary.
+-}
+getMaybeRange :: Get (Maybe Range)
+getMaybeRange =
+  getMaybeRangeTag >>= \case
+    TagNothing ->
+      pure Nothing
+    TagJustRange'Point -> do
+      line <- decodeLEB128 getWord8
+      column <- decodeLEB128 getWord8
+      pure . Just $! Range'Point{..}
+    TagJustRange'OneLine -> do
+      line <- decodeLEB128 getWord8
+      column <- decodeLEB128 getWord8
+      endColumn <- decodeLEB128 getWord8
+      pure . Just $! Range'OneLine{..}
+    TagJustRange'MultiLine -> do
+      line <- decodeLEB128 getWord8
+      column <- decodeLEB128 getWord8
+      endLine <- decodeLEB128 getWord8
+      endColumn <- decodeLEB128 getWord8
+      pure . Just $! Range'MultiLine{..}
