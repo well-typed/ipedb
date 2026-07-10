513c9f8fef1ba6a01f210bb536ab09dc75cd9fda
Wen Kokke <wenkokke@users.noreply.github.com>
2026-07-10 12:04:38 +0100

    Add HashMap instance.

--- src/IpeDB/Database.hs
+++ src/IpeDB/Database.hs
@@ -1,4 +1,5 @@
 module IpeDB.Database (
+  HashMap,
   LSMTree,
 ) where
 
@@ -6,13 +7,48 @@ import Data.Machine (ProcessT, SourceT)
 import Data.Vector (Vector)
 import Data.Void (Void)
 import IpeDB.Database.Class (Database (..))
+import IpeDB.Database.HashMap qualified as HashMap
 import IpeDB.Database.LSMTree qualified as LSMTree
+
+data HashMap
+
+instance Database HashMap where
+  type Key HashMap k = HashMap.Key k
+  type Value HashMap v = HashMap.Value v
+
+  type SessionOptions HashMap = HashMap.SessionOptions
+  type Session HashMap = HashMap.Session
+
+  withNewSession :: SessionOptions HashMap -> (Session HashMap -> IO r) -> IO r
+  withNewSession = HashMap.withNewSession
+
+  type TableOptions HashMap = HashMap.TableOptions
+  type Table HashMap k v = HashMap.Table k v
+
+  withNewTable :: Session HashMap -> TableOptions HashMap -> (Table HashMap k v -> IO r) -> IO r
+  withNewTable = HashMap.withNewTable
+
+  insert :: (Key HashMap k) => Table HashMap k v -> k -> v -> IO ()
+  insert = HashMap.insert
+
+  lookup :: (Key HashMap k) => Table HashMap k v -> k -> IO (Maybe v)
+  lookup = HashMap.lookup
+
+  type IteratorOptions HashMap = HashMap.IteratorOptions
+
+  withIterator :: IteratorOptions HashMap -> Table HashMap k v -> (SourceT IO (k, v) -> IO a) -> IO a
+  withIterator = HashMap.withIterator
+
+  type IndexerOptions HashMap = HashMap.IndexerOptions
+
+  indexer :: (Key HashMap k) => (e -> Maybe (k, v)) -> IndexerOptions HashMap -> Table HashMap k v -> ProcessT IO e Void
+  indexer = HashMap.indexer
 
 data LSMTree
 
 instance Database LSMTree where
-  type Key LSMTree = LSMTree.Key
-  type Value LSMTree = LSMTree.Value
+  type Key LSMTree k = LSMTree.Key k
+  type Value LSMTree v = LSMTree.Value v
 
   type SessionOptions LSMTree = LSMTree.SessionOptions
   type Session LSMTree = LSMTree.Session
